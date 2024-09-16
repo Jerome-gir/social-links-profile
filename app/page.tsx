03a8593081e0349d3aad7c905dcd7843a06937d2
Jerome <jerome.gir@gmail.com>
2024-09-17 01:33:43 +0200

    desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+import Image from "next/image"
+import Link from "next/link" // Import de Link
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  type Links = {
+    name: string
+    link?: string
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const socialLinks: Links[] = [
+    {
+      name: "Github",
+      link: "https://github.com/Jerome-gir",
+    },
+    {
+      name: "Frontend Mentor",
+      link: "https://www.frontendmentor.io/profile/Jerome-gir",
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/jerome-girard-21838783/",
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com/jeromegir",
+    },
+    {
+      name: "Instagram",
+    },
+  ]
+
+  return (
+    <main className="bg-grey900 min-h-screen flex items-center justify-center">
+      <div className="w-[400px] h-[600px] bg-grey800 rounded-xl p-10">
+        <div className="flex items-center flex-col">
+          <Image
+            className="rounded-full mb-6"
+            src="/avatar-jessica.jpeg"
+            alt="profile-photo"
+            width={90}
+            height={90}
+          />
+          <h1 className="text-white font-interBold text-2xl tracking-wide mb-2">
+            Jessica Randall
+          </h1>
+          <p className="text-green font-interBold text-sm tracking-wide mb-6">
+            London, United Kingdom
+          </p>
+          <p className="text-white font-inter mb-6">
+            &quot;Front-end developer and avid reader.&quot;
+          </p>
+          <ul className=" flex flex-col gap-4 cursor-pointer">
+            {socialLinks.map((link, index) => (
+              <li
+                key={index}
+                className="w-80 h-11 rounded-lg bg-grey700 text-white flex items-center justify-center font-interBold hover:bg-green hover:text-grey900"
+              >
+                {link.link ? (
+                  <Link href={link.link}>{link.name}</Link>
+                ) : (
+                  link.name
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    </main>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config: Config = {
   content: [
@@ -8,12 +9,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["var(--font-inter-regular)", ...fontFamily.sans],
+        interSemiBold: ["var(--font-inter-semibold)", ...fontFamily.sans],
+        interBold: ["var(--font-inter-bold)", ...fontFamily.sans],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        green: "var(--green)",
+        white: "var(--white)",
+        grey700: "var(--grey700)",
+        grey800: "var(--grey800)",
+        grey900: "var(--grey900)",
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
